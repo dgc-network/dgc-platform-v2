a307e130440737b2ed06231b270bf90a9a57b4bf
Rover <rover@RoverdeMacBook-Pro.local>
2020-05-13 21:27:37 +0800

    rust-crypto = "0.2"

--- daemon/src/rest_api/routes/agents.rs
+++ daemon/src/rest_api/routes/agents.rs
@@ -158,3 +158,58 @@ pub async fn update_agent(
         .map(|agents| HttpResponse::Ok().json(agents))
 }
 
+//use crate::error::CliError;
+//use crate::http::submit_batches;
+use crate::rest_api::transaction::{pike_batch_builder, PIKE_NAMESPACE};
+use grid_sdk::{
+    protocol::pike::payload::{Action, CreateAgentAction, PikePayloadBuilder, UpdateAgentAction},
+    protos::IntoProto,
+};
+
+pub async fn do_create_agent(
+    url: &str,
+    key: Option<String>,
+    wait: u64,
+    create_agent: CreateAgentAction,
+    service_id: Option<String>,
+) -> Result<(), CliError> {
+    let payload = PikePayloadBuilder::new()
+        .with_action(Action::CreateAgent)
+        .with_create_agent(create_agent)
+        .build()
+        .map_err(|err| CliError::UserError(format!("{}", err)))?;
+
+    let batch_list = pike_batch_builder(key)
+        .add_transaction(
+            &payload.into_proto()?,
+            &[PIKE_NAMESPACE.to_string()],
+            &[PIKE_NAMESPACE.to_string()],
+        )?
+        .create_batch_list();
+
+    submit_batches(url, wait, &batch_list, service_id.as_deref())
+}
+
+pub async fn do_update_agent(
+    url: &str,
+    key: Option<String>,
+    wait: u64,
+    update_agent: UpdateAgentAction,
+    service_id: Option<String>,
+) -> Result<(), CliError> {
+    let payload = PikePayloadBuilder::new()
+        .with_action(Action::UpdateAgent)
+        .with_update_agent(update_agent)
+        .build()
+        .map_err(|err| CliError::UserError(format!("{}", err)))?;
+
+    let batch_list = pike_batch_builder(key)
+        .add_transaction(
+            &payload.into_proto()?,
+            &[PIKE_NAMESPACE.to_string()],
+            &[PIKE_NAMESPACE.to_string()],
+        )?
+        .create_batch_list();
+
+    submit_batches(url, wait, &batch_list, service_id.as_deref())
+}
