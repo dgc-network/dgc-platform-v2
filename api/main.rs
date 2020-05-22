e97cfa2b541524e38e7949cbcd8f899cb0522d5d
Rover <rover@RoverdeMacBook-Pro.local>
2020-05-22 21:08:07 +0800

    api

---
+++ api/main.rs
@@ -0,0 +1,24 @@
+// Copyright (c) The dgc.network
+// SPDX-License-Identifier: Apache-2.0
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn index() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+async fn index2() -> impl Responder {
+    HttpResponse::Ok().body("Hello world again!")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+            .route("/again", web::get().to(index2))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
